da7f0828841f0d4f69709907856c1066f6050df5
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-01-08 22:13:28 +0500

    initial commit

---
+++ src/Components/FriendList.js
@@ -0,0 +1,9 @@
+function FriendList() {
+    return (
+        <div>
+            Friend List
+        </div>
+    )
+}
+
+export default FriendList
---
+++ src/Components/Friends.js
@@ -0,0 +1,9 @@
+function Friends() {
+    return (
+        <div>
+            Friends
+        </div>
+    )
+}
+
+export default Friends
